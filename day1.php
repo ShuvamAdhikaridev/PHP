bbf4eecdca646320074f817ab0921c52227785ab
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-02-05 15:42:03 +0545

    Data_Types

--- day1.php
+++ day1.php
@@ -9,16 +9,23 @@
 
 <body>
     <?
-    $characterAge = 21;
-    $characterName = "Shvuam";
-
-
+    // $characterAge = 21;
+    // $characterName = "Shvuam";
 
     // echo "<h1> Hello </h1>";
     // echo "<hr>";
     // echo "<p>This is my site</p>";
-    echo "My name is $characterName <br>";
-    echo "Any My age is $characterAge";
+    // echo "My name is $characterName <br>";
+    // $characterName = "Hello";
+    // echo "Any My age is $characterAge";
+    // echo "My name is $characterName <br>";
+
+    $phrase = "To be or not to be";
+    $age = 21;
+    $gpa = 3.35;
+    $isLoggedIn = false;
+    //? Null
+
 
     ?>
 
